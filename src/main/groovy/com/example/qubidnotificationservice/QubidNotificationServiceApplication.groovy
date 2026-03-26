c53ca6f3ce7890a6afd1b44deebe38dfad2c84ba
patel-neil <neilpatel400271@gmail.com>
2026-03-26 14:04:54 +0530

    Initial project setup

---
+++ src/main/groovy/com/example/qubidnotificationservice/QubidNotificationServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.qubidnotificationservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class QubidNotificationServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(QubidNotificationServiceApplication, args)
+    }
+
+}
